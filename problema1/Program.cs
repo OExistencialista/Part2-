8eddab4d8f9b9d449963bea3bb8f570a2315e330
OExistencialista <134657253+OExistencialista@users.noreply.github.com>
2024-06-26 08:53:16 -0300

    problema 1

---
+++ problema1/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program{
+    public static void Main(){
+        Console.Clear();
+        
+        decimal valorInicial = 1000;
+        Console.WriteLine("Valor inicial:" + valorInicial);
+        
+        decimal taxa = 5.30M;
+        Console.WriteLine("Taxa:"+ taxa);
+        
+        int periodo = 10;
+        Console.WriteLine("Periodo:" + periodo);
+
+
+
+        Console.Write($"Valor final:{juroscompostos(valorInicial, taxa,periodo):F2}");
+
+    }
+    public static decimal juroscompostos(decimal vlPresente, decimal taxa, int periodo){
+        return vlPresente * Pow(1+taxa/100,periodo);
+    }
+    public static decimal Pow(decimal num, int power){
+        if(power ==1 ) return num;
+        if(power>1) return Pow(num, power-1) * num;
+        else return Pow(num, power+1)/num;
+    }
+
+}
